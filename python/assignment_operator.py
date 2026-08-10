8699e1216a62d8812af76d441af907dfc6b3c04d
akashsingh0202 <akashsinghsinghraul@gmail.com>
2026-08-10 14:02:35 +0530

    sec

---
+++ python/assignment_operator.py
@@ -0,0 +1,63 @@
+# Assignment Operator
+x = 10
+print("=  :", x)
+
+# Addition Assignment
+x = 10
+x += 5
+print("+= :", x)
+
+# Subtraction Assignment
+x = 10
+x -= 5
+print("-= :", x)
+
+# Multiplication Assignment
+x = 10
+x *= 5
+print("*= :", x)
+
+# Division Assignment
+x = 10
+x /= 5
+print("/= :", x)
+
+# Floor Division Assignment
+x = 10
+x //= 3
+print("//=:", x)
+
+# Modulus Assignment
+x = 10
+x %= 3
+print("%= :", x)
+
+# Exponentiation Assignment
+x = 2
+x **= 3
+print("**=:", x)
+
+# Bitwise AND Assignment
+x = 10
+x &= 3
+print("&= :", x)
+
+# Bitwise OR Assignment
+x = 10
+x |= 3
+print("|= :", x)
+
+# Bitwise XOR Assignment
+x = 10
+x ^= 3
+print("^= :", x)
+
+# Right Shift Assignment
+x = 10
+x >>= 1
+print(">>=:", x)
+
+# Left Shift Assignment
+x = 10
+x <<= 1
+print("<<=:", x)
